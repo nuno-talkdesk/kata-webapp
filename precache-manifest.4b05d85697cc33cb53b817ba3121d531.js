8db117f1cc55e2cc5a059e70ab08ef7cc35f817d
Nuno Lages <nuno.lages@talkdesk.com>
2020-10-23 10:38:08 +0100

    Updates

---
+++ precache-manifest.4b05d85697cc33cb53b817ba3121d531.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b4cbf90677cc0ab26c4cc29c24730cb",
+    "url": "/kata-webapp/index.html"
+  },
+  {
+    "revision": "24ab790221b9b3f7e067",
+    "url": "/kata-webapp/static/css/main.15cc8519.chunk.css"
+  },
+  {
+    "revision": "64de7dd76c3d9d25cf79",
+    "url": "/kata-webapp/static/js/2.7a5c50b9.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/kata-webapp/static/js/2.7a5c50b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ab790221b9b3f7e067",
+    "url": "/kata-webapp/static/js/main.95156e7d.chunk.js"
+  },
+  {
+    "revision": "7a58b638a7a42ae6b1a5",
+    "url": "/kata-webapp/static/js/runtime-main.62a8ebdd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kata-webapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "174d6240ccaa2abf312334b54cf84ee5",
+    "url": "/kata-webapp/static/media/nepal-in-pictures-beautiful-places-to-photograph-annapurna-range.174d6240.jpg"
+  }
+]);
